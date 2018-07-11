70138b965960c492b8cd3ce1aabf95140024ac17
Akshi Agarwal <akshiagarwal@gmail.com>
2018-07-11 10:31:38 -0700

    Create SummaryRanges.go

---
+++ SummaryRanges.go
@@ -0,0 +1,9 @@
+func summaryRanges(nums []int) []string {
+    arr := []String{}
+    i := 0;
+        for i < nums.length-1 {
+            if nums[i] == nums[i+1] - 1 {
+                str : 
+            }
+        }
+}
