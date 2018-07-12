cd50f3521896d4c78abceb45615819fa57eabf21
Akshi Agarwal <akshiagarwal@gmail.com>
2018-07-12 09:56:25 -0700

    Delete SummaryRanges.go

--- SummaryRanges.go
+++
@@ -1,9 +0,0 @@
-func summaryRanges(nums []int) []string {
-    arr := []String{}
-    i := 0;
-        for i < nums.length-1 {
-            if nums[i] == nums[i+1] - 1 {
-                str : 
-            }
-        }
-}
